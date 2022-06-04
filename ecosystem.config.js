d4d6a2e504ea44dae4bef52ccf09800143938b30
Deshario <deshario9@gmail.com>
2022-06-04 20:30:01 +0700

    Refactor slug generator

--- ecosystem.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    apps : [{
-      name   : "sendight",
-      script : "./dist/app.js"
-    }]
-  }
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,21 @@
 import express from 'express';
 import http, { Server } from 'http';
 import cors from 'cors';
-
-import { createSlug } from './controllers/slugController';
 import { initSocket } from './sockets';
 
 const app = express();
-const server: Server = http.createServer(app);
 const port = 8000;
+const server: Server = http.createServer(app);
 
 app.use(cors());
 
 initSocket(server);
 
-app.get('/slug', (req, res, next) => {
-  createSlug(req, res, next).then();
+app.get('/slug', (req, res) => {
+  const slug = Math.random().toString(36).substring(2, 9);
+  res.status(200).json({ message: 'Success', data: { slug } });
 });
 
 server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  return console.log('Server listening at port: ', port);
+  console.log(`> Server listening at port: ${port} <`);
 });
